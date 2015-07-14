6c6d880ec06795c893b4ed15c79afeac79dad6b5
Tinckle Trivedi <tinckle.trivedi@infibeam.net>
2015-07-14 14:19:52 +0530

    fourth

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -1,2 +1,29 @@
 class RegisterController < ApplicationController
+  def show
+  	#@register = User.find(params[:id])
+  	
+  end
+
+  def newuser
+  end
+
+  def login
+  end
+
+  def submit
+    temp = params[:login]
+    @login = User.where("address = ? and password = ?", temp[:address], temp[:password])
+  end
+
+  def create
+  	@register = User.new(user_params)
+
+    @register.save
+  	redirect_to register_path(@register)
+  end
+
+  private
+  	def user_params
+  		params.require(:register).permit(:name, :address, :password)  		
+  	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  post 'register/newuser'
+  post 'register/login'
+  post 'register/submit'
+  resources :register
 
 get "blog_posts/search"
 post "blog_posts/results"
